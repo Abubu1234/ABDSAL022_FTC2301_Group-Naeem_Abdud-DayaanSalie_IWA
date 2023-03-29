553f43d81e050eafe957910d2ce912499ca688a6
Abdud-Dayaan <abduddayaansalie@gmail.com>
2023-03-29 15:58:00 +0200

    imported variables with changed names

---
+++ IWA3/challenge 2/js/script.js
@@ -0,0 +1,15 @@
+// scripts.js
+
+import  {firstnameA}  from "./alex.js"
+import  {surnameA} from "./alex.js"
+import {roleA} from "./alex.js"
+
+import  {firstnameJ}  from "./johannes.js"
+import  {surnameJ} from "./johannes.js"
+import {roleJ} from "./johannes.js"
+
+import  {firstnameN}  from "./nwabisa.js"
+import  {surnameN} from "./nwabisa.js"
+import {roleN} from "./nwabisa.js"
+
+console.log(roleN, roleJ, roleA)
