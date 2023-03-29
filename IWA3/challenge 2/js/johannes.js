3887017c7a83788b4ceb8fea0642122cb094434e
Abdud-Dayaan <abduddayaansalie@gmail.com>
2023-03-29 15:57:43 +0200

    changed variable names and exported them

---
+++ IWA3/challenge 2/js/johannes.js
@@ -0,0 +1,9 @@
+// johannes.js
+
+
+export const firstnameJ = "Johannes"
+export const surnameJ = "Potgieter"
+export const roleJ = "Intern"
+
+const display= firstnameJ + " "+ surnameJ +" ("+ roleJ + ")"
+document.querySelector('#johannes').innerText = display
