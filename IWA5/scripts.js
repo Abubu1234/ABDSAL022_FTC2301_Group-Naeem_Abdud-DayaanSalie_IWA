a2c0f40aaa646e0154d514195eb2b68067c6ba42
Abdud-Dayaan <abduddayaansalie@gmail.com>
2023-04-03 21:57:31 +0200

    altered code

--- IWA5/scripts.js
+++ IWA5/scripts.js
@@ -1,37 +1,53 @@
 FREE_WARNING = 'Free shipping only applies to single customer orders'
 BANNED_WARNING = 'Unfortunately we do not ship to your country of residence'
-NONE_SELECTED = '0'
+NONE_SELECTED = 0
 
-if (location = RSA) 
-{ shipping === 400 && currency === 'R' }
+let location = "RSA" || "NAM"
+let shipping = null
+let currency = "R" || "$"
+let customers = 1
+const price = shipping + total
 
-if ( location = NAM){
-    shipping = 600
-} else{
-    shipping = 800
+if (location = "RSA") {
+	shipping === 400
+	currency === "R" 
+} else if (location === "NAM") {
+	shipping = 600
+	currency = "$"
+} else {
+	shipping = 800
+}
+
+if ( location = "NAM") {
+	shipping = 600 && currency === "$"
+} 
+if (shipping = 800) {
+	location = "NK"
 }
 
 shoes = 300 * 1
-toys - 100 * 5
-shirts = 150 * 'NONE_SELECTED'
+toys = 100 * 5
+shirts = 150 * NONE_SELECTED
 batteries = 35 * 2
-pens = 5 * 'NONE_SELECTED' 
+pens = 5 * NONE_SELECTED 
 
-shipping = null
-currency = $
+var total = (shoes + toys + shirts + batteries + pens)
 
-if (shoes + batteries + pens + shirts > 1000) {
-	if (location = NAM && customers < 2) {
-			if (location = RSA)
-		    shipping = 0 || calcShipping
-		}
-	}
+if (total > 1000) {
+	shipping = 0 || calcShipping
+}
 
+if (shipping = 0 && customers !== 1  ){ 
+	console.log(FREE_WARNING) 
+}
+    
 
-    console.log(FREE_WARNING) ? shipping = 0 && customers === 1  
+if (location === "NK"){ 
+	console.log(total, currency, shoes + batteries + pens + shirts + shipping) 
+	
+} else {
+	console.log(BANNED_WARNING)  
+}
 
-(location === 'NK') ? console.log(WARNING) : console.log('price', currency, shoes + batteries + pens + shirts + shipping)
-
-customers = '1'
-const location = 'RSA'
+customers = 1
 currency = null
