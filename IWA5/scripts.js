6ceab5893420aa374370fcc76cc4067e614b1320
Abdud-Dayaan <abduddayaansalie@gmail.com>
2023-04-03 13:55:23 +0200

    created js script and changing to make it work

---
+++ IWA5/scripts.js
@@ -0,0 +1,37 @@
+FREE_WARNING = 'Free shipping only applies to single customer orders'
+BANNED_WARNING = 'Unfortunately we do not ship to your country of residence'
+NONE_SELECTED = '0'
+
+if (location = RSA) 
+{ shipping === 400 && currency === 'R' }
+
+if ( location = NAM){
+    shipping = 600
+} else{
+    shipping = 800
+}
+
+shoes = 300 * 1
+toys - 100 * 5
+shirts = 150 * 'NONE_SELECTED'
+batteries = 35 * 2
+pens = 5 * 'NONE_SELECTED' 
+
+shipping = null
+currency = $
+
+if (shoes + batteries + pens + shirts > 1000) {
+	if (location = NAM && customers < 2) {
+			if (location = RSA)
+		    shipping = 0 || calcShipping
+		}
+	}
+
+
+    console.log(FREE_WARNING) ? shipping = 0 && customers === 1  
+
+(location === 'NK') ? console.log(WARNING) : console.log('price', currency, shoes + batteries + pens + shirts + shipping)
+
+customers = '1'
+const location = 'RSA'
+currency = null
