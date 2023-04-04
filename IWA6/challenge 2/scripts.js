e6af442ae3f2ad7b9796db94e43ddfecb737027b
Abubu1234 <abduddayaansalie@gmail.com>
2023-04-04 16:01:00 +0200

    attempting to solve code

---
+++ IWA6/challenge 2/scripts.js
@@ -0,0 +1,18 @@
+
+const rent = 400;
+const tax = '12%';
+const food = 51.7501;
+const salary = 800;
+const transport = 10.2;
+const hourOfDay = 00;
+const minuteOfDay = 00;
+
+// Only change below this line
+
+if (hourOfDay = undefined && minuteOfDay = undefined) || (hourOfDay == '00'&& minuteOfDay == '00') {
+	const taxAsDecimal = tax / '100'
+  const startingAfterTax = salary * '1' - taxAsDecimal
+	const balance = starting - transport - food - rent
+}
+
+console.log(balance.toFixed(2)) 
