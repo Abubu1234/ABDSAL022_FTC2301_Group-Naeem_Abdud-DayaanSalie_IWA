a98e29ff7de87f4a96abc63defaa22107a3bb7b7
Abubu1234 <abduddayaansalie@gmail.com>
2023-04-05 11:00:04 +0200

    removed conflctiing code

--- IWA5/scripts.js
+++ IWA5/scripts.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const FREE_WARNING = 'Free shipping only applies to single customer orders'
 const BANNED_WARNING = 'Unfortunately we do not ship to your country of residence'
 const NONE_SELECTED = 0
