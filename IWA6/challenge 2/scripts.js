8f3ef0b3a8e0c47fb7193955d323fa0933f462b8
Abubu1234 <abduddayaansalie@gmail.com>
2023-04-05 14:09:01 +0200

    moved variables out of {}

--- IWA6/challenge 2/scripts.js
+++ IWA6/challenge 2/scripts.js
@@ -1,18 +1,18 @@
-
 const rent = 400;
-const tax = '12%';
+const tax = '8%';
 const food = 51.7501;
 const salary = 800;
 const transport = 10.2;
 const hourOfDay = 00;
 const minuteOfDay = 00;
 
-// Only change below this line
+// Only change beconst taxAsDecimal = tax / '100';
+const taxAsDecimal = tax / '100';
+const startingAfterTax = salary * '1' - taxAsDecimal;
+const balance = starting - transport - food - rent
 
-if (hourOfDay = undefined && minuteOfDay = undefined) || (hourOfDay == '00'&& minuteOfDay == '00') {
-	const taxAsDecimal = tax / '100'
-  const startingAfterTax = salary * '1' - taxAsDecimal
-	const balance = starting - transport - food - rent
+if (hourOfDay !== null) && (minuteOfDay !== null) && (hourOfDay == '00') && (minuteOfDay == '00') {
+
 }
-
-console.log(balance.toFixed(2)) 
+	
+console.log(balance .toFixed(2))
