7668fcbd4aa3d7385c6ead472e493888f6122357
Abubu1234 <abduddayaansalie@gmail.com>
2023-04-06 11:02:22 +0200

    solved code

--- IWA7/challenge 3/scripts.js
+++ IWA7/challenge 3/scripts.js
@@ -10,10 +10,10 @@ const divider = '----------------------------------'
 
 // Only change below this line
 
-const owed = (-parseInt(leoBalance) + -parseFloat(sarahBalance)).toFixed(2)
-const leo = `${leoName} ${leoSurname.trimEnd()} (Owed: R ${-leoBalance})`
-const sarah =`${sarahName.trimEnd()} ${sarahSurname} (Owed:R${-sarahBalance})`
+const owed =(-parseInt(leoBalance).toFixed(2) )+ (-parseFloat(sarahBalance).toFixed(2))
+const leo = `${leoName} ${leoSurname.trimEnd()} (Owed: R ${parseFloat(-leoBalance).toFixed(2)})`
+const sarah =`${sarahName.trimEnd()} ${sarahSurname} (Owed:R${parseInt(-sarahBalance).toFixed(2)})`
 const total = `Total amount owed:` 
-const result = `${leo}\n ${sarah}\n\n${divider}\n ${total}\t${owed}\n ${divider}`
+const result = `${leo}\n${sarah}\n\n${divider}\n${total}\t${owed}\n ${divider}`
 
 console.log(result)
