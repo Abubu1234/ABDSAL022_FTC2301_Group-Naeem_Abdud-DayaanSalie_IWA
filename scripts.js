d45b161b490f8746b0524f3a3aff628cc2a4d285
Abubu1234 <abduddayaansalie@gmail.com>
2023-04-06 11:59:29 +0200

    created js script and index for script

---
+++ scripts.js
@@ -0,0 +1,38 @@
+const salary = 4000;
+const lodging = 'apartment'
+const size = 'large'
+
+
+// Only change the syntax below (not the values or key names)
+
+const expenses = {
+    food: 51.7501,
+    transport:  10.2,
+}
+  
+const tax = {
+    734: '3%',
+    234: '20%',
+    913: '12%',
+    415: '38%',
+    502: '42%',
+}
+
+const rent = {
+    none: 0,
+    'small-room': 200,
+    'large-room': 300,
+    'small-apartment': 400,
+    'large-apartment': 800,
+    'small-house': 1200,
+    'large-house': 2400,
+}
+
+// You can change below however you want
+
+const taxAsDecimal = tax 913 / 100
+const startingAfterTax = salary * 1 - taxAsDecimal
+const type = lodging + size
+const balance = expenses(transport) - expenses(food) - expenses(rent.type) 
+console.log(balance)
+ 
