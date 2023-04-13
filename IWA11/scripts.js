c057a568e9e04d6ae1d2d1b9ea9b17e929a480ef
Abubu1234 <abduddayaansalie@gmail.com>
2023-04-13 09:34:28 +0200

    attempting to solve challenge 11

---
+++ IWA11/scripts.js
@@ -0,0 +1,34 @@
+// script.js
+
+const root1 = document(order1)
+const biscuits1 = document(biscuits1)
+const donuts1 = document(donuts1)
+const pancakes1 = document(pancakes1)
+const status1 = document(status1)
+
+const root2 = document(order2)
+const biscuits2 = document(biscuits2)
+const donuts2 = document(donuts2)
+const pancakes2 = document(pancake2s)
+const status2 = document(status2)
+
+const root3 = document(order3)
+const biscuits3 = document(biscuit3)
+const donuts3 = document(donuts3)
+const pancakes3 = document(pancakes3)
+const status3 = document(status3)
+
+biscuits1 = root1.biscuits1,
+donuts1 = root1.donuts1,
+pancakes1 = root1.pancakes1,
+status1 = root1.status1 ? Delivered : Pending
+
+biscuits2= root2.biscuits2,
+donuts2 = root2.donuts2,
+pancakes2 = root2.pancakes2,
+status2 = roo2t.status2 ? Delivered : Pending
+
+biscuits3 = root3.biscuits3,
+donuts3 = root3.donuts3,
+pancakes3 = root3.pancakes3,
+status3 = root3.status3 ? Delivered : Pending
