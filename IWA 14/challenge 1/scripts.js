9dae3d2f704b8facb0ca0c06cdc3fb6733161998
Abdud-Dayaan <abduddayaansalie@gmail.com>
2023-04-25 01:53:30 +0200

    Solved IWA 14 challenge 1 debugging challenge 2

--- IWA 14/challenge 1/scripts.js
+++ IWA 14/challenge 1/scripts.js
@@ -1,14 +1,15 @@
-firstName = 'John';
-age = 35;
-hobby = 'Coding';
+const firstName = 'John';
+const age = 35;
+const hobbies = 'Coding';
 
-const logTwice = () => {
-  console(parameter)
-  console(parameter)
+const logTwice = (parameter) => {
+  console.log(parameter)
+  console.log(parameter)
 }
 
 function hobby () {
-  logTwice(`Hello, ${name} (${age}). I love ${hobby}!`)
+  logTwice(`Hello, ${firstName} (${age}). I love ${hobbies}!`)
 }
 
 hobby()
+
--- IWA 14/challenge 2/scripts.js
+++ IWA 14/challenge 2/scripts.js
@@ -1,38 +1,34 @@
-// script.js
-
-function add = a, b => { a + b }
-
-function multiply = a, b => { a - b }
-
-function internal() {
-	const added = this.add(this.a, this.b)
-	this.multiply(this.a, this.b)
-	return this
-}
-
-// Not allowed to change below this
-
-const example1 = {
+const calculator = {
+	add: (a, b) => { return a + b },
+	multiply: (a, b) => { return a * b },
+	internal: function() {
+	  const added = this.add(this.internal.a, this.internal.b)
+	  const multiplied = this.multiply(this.internal.a, this.internal.b)
+	  this.internal.result = added + multiplied
+	  return this
+	}
+  }
+  
+  const example1 = {
 	internal: {
-		a: 2,
-		b: 4,
-		c: 8,
+	  a: 2,
+	  b: 4,
+	  c: 8,
 	},
-	add,
-	multiply,
-  calculate: internal
-}
-
-const example2 = {
+	calculate: calculator.internal,
+  }
+  
+  const example2 = {
 	internal: {
-		a: 2,
-		b: 2,
-		c: 3,
+	  a: 2,
+	  b: 2,
+	  c: 3,
 	},
-	add,
-	multiply,
-  calculate: internal
-}
-
-example1.calculate()
-example2.calculate()
+	calculate: calculator.internal,
+  }
+  
+  example1.calculate()
+  console.log(example1.internal.result)
+  
+  example2.calculate()
+  console.log(example2.internal.result) 
