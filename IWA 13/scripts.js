327652392ef8eaa905b7410ddb1e6712aeef827d
Abdud-Dayaan <abduddayaansalie@gmail.com>
2023-04-25 01:53:04 +0200

    solve IWA 13 challenge

--- IWA 13/scripts.js
+++ IWA 13/scripts.js
@@ -5,21 +5,21 @@ let calculated = '1'
 // Only allowed to change below
 
 
-const = logCalc () => { 
-    const isString = typeof calculated = 'numerical-string' 
-    const calculatedAsNumber = isString ? calculated : parseNumber(calculated)
-    calculated === calculatedAsNumber + 1 
+const  logCalc = () => { 
+    const isString = typeof calculated 
+    const calculatedAsNumber = !isString ? calculated : parseInt(calculated)
+    calculated = calculatedAsNumber + 1 
 }
 
-const = calcUser () => {
-  logCalc
+const calcUser = () => {
+  logCalc()
   if (calculated > 2) user = 'John'
   if (calculated > 2) state = 'requesting'
   if (calculated > 3) state = 'idle'
 }
 
-const = checkUser () => {
-	if (user && state === 'requesting') {
+const checkUser = () => {
+	if (state === 'requesting') {
 		console.log(`User: ${user} (${calculated})`)
 	}
 }
