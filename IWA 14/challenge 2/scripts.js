60182312260a72dd5eeb5e516da56b46097534c3
Abubu1234 <abduddayaansalie@gmail.com>
2023-05-03 12:46:34 +0200

    completed challenges

--- IWA 14/challenge 2/scripts.js
+++ IWA 14/challenge 2/scripts.js
@@ -1,34 +1,40 @@
-const calculator = {
-	add: (a, b) => { return a + b },
-	multiply: (a, b) => { return a * b },
-	internal: function() {
-	  const added = this.add(this.internal.a, this.internal.b)
-	  const multiplied = this.multiply(this.internal.a, this.internal.b)
-	  this.internal.result = added + multiplied
-	  return this
-	}
-  }
-  
-  const example1 = {
+const add = (a, b) => {
+	return a + b 
+}
+
+const multiply = (a, b) => { 
+	return a * b 
+}
+
+function internal() {
+	const added = this.add(this.internal.a, this.internal.b)
+	const multiplied = this.multiply(added, this.internal.c)
+	console.log(multiplied) 
+}
+
+// Not allowed to change below this
+
+const example1 = {
 	internal: {
-	  a: 2,
-	  b: 4,
-	  c: 8,
+		a: 2,
+		b: 4,
+		c: 8,
 	},
-	calculate: calculator.internal,
-  }
-  
-  const example2 = {
+	add,
+	multiply,
+  calculate: internal
+}
+
+const example2 = {
 	internal: {
-	  a: 2,
-	  b: 2,
-	  c: 3,
+		a: 2,
+		b: 2,
+		c: 3,
 	},
-	calculate: calculator.internal,
-  }
-  
-  example1.calculate()
-  console.log(example1.internal.result)
-  
-  example2.calculate()
-  console.log(example2.internal.result) 
+	add,
+	multiply,
+  calculate: internal
+}
+
+example1.calculate()
+example2.calculate()
