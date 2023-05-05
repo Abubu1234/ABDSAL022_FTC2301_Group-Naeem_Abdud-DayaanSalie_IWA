5e73592dee6cc205e86936404f04d1778a4fb8e1
Abubu1234 <abduddayaansalie@gmail.com>
2023-05-05 11:29:01 +0200

    solved challenge 18

--- IWA 18/scripts.js
+++ IWA 18/scripts.js
@@ -30,52 +30,79 @@ const handleDragOver = (event) => {
     updateDraggingHtml({ over: column })
 }
 
+// created variable dragging for when order is being dragged in the global scope
+var Dragging;
+//created variable  
+/**
+ * This function is called when a user starts dragging an order.
+ * It sets the active dragging column in the state object (data.js) 
+ * to the column over which the order was originally located.
+ * @param {Event} event 
+ */
 const handleDragStart = (event) => {
-    
+    Dragging = event.target.closest('.order')
+    state.dragging.source = state.dragging.over
+    id = beingDragged.dataset.id
 }
-  
+
+/**
+ * This function is called when a user stops dragging an order.
+ * It moves the order to the new column and updates the HTML.
+ * @param {Event} event 
+ */
 const handleDragEnd = (event) => {
-    
+    event.preventDefault()
+    const move = state.dragging.over
+    moveToColumn(id, move)
+    updateDraggingHtml({ over: column })
 }
-  
-  
+
 
 const handleHelpToggle = (event) => {
-    const { target } = event;
-
-    if (target == html.other.help) {
-        html.help.overlay.showModal()
-    }
-
-    if (target == html.help.cancel) {
-        html.help.overlay.close()
-    }
+// modal shows dialog of hidden text 
+    html.help.overlay.showModal()
+    // moved html.help.cancel.addEventListener('click') into handleHelpToggl
+    html.help.cancel.addEventListener('click',() => html.help.overlay.close())
 }
+html.other.help.addEventListener('click', handleHelpToggle)
 
 const handleAddToggle = (event) => {
-    const { target } = event;
-
-    if (target == html.other.add) {
-        html.add.overlay.showModal()
-    }
-
-    if (target == html.add.cancel) {
-        html.add.overlay.close()
-    }
+    html.add.overlay.showModal()
+    html.add.cancel.addEventListener('click',() => html.add.overlay.close())
 }
+html.other.add.addEventListener('click', handleAddToggle)
 
+/**
+ * When the "Add" button in the add Order overlay is clicked, then this 
+ * will handle the addition of the new order to the "Ordered" column. 
+ * @param {Event} event 
+ */
 const handleAddSubmit = (event) => {
-    event.preventDefault()
-    const title = event.target.elements.title.value
-    const table = event.target.elements.table.value
-    const column = 'ordered'
-    const order = createOrderData({ title, table, column })
-    state.orders[order.id] = order
-    const element = createOrderHtml(order)
-    const columnContainer = document.querySelector(`[data-column="${column}"]`)
-    columnContainer.appendChild(element)
-    html.add.overlay.close()
-}   
+    event.preventDefault();
+
+      /* created the props object required for the createOrderData function */
+      const props = {
+        title: html.add.form.title.value,
+        table: html.add.form.table.value,
+        column: html.edit.column.value
+    };
+    const orderData = createOrderData(props);
+    //create a new object 
+    const order = orderData;
+    const orderHtml = createOrderHtml(order);
+    //to move the order to the "ordered" column
+    const orderedColumn = html.columns.or                 dered;
+    orderedColumn.appendChild(orderHtml);
+    //makes the form empty after submission
+    html.add.form.reset();
+
+    // will close the overlay before adding the orders to html
+    html.add.overlay.close();
+    html.other.add.focus();
+
+}
+html.add.form.addEventListener('submit', handleAddSubmit)
+
 
 const handleEditToggle = (event) => {
     const { target } = event;
@@ -91,6 +118,14 @@ const handleEditToggle = (event) => {
     }
 }
 
+/**
+ * This function is called when a user submits the "Edit Order".
+ * It extracts the data entered in the form, removes the old order,
+ * creates a new order data object, creates a new HTML element, and
+ * appends the HTML element.
+ * .value is used to access the 'title', 'table', and 'column' input.
+ * @param {Event} event 
+ */
 const handleEditSubmit = (event) => {
     event.preventDefault();
     const order = document.querySelector('.order')
@@ -107,6 +142,12 @@ const handleEditSubmit = (event) => {
     html.edit.overlay.close()
 }
 
+/**
+ * This function is called when a user clicks "delete" on
+ * the "Edit Order" form.
+ * It removes the order and close the modal.
+ * @param {Event} event 
+ */
 const handleDelete = (event) => {
     const { target } = event
 
@@ -117,17 +158,17 @@ const handleDelete = (event) => {
     html.edit.overlay.close()
 }
 
-html.add.cancel.addEventListener('click', handleAddToggle)
-html.other.add.addEventListener('click', handleAddToggle)
-html.add.form.addEventListener('submit', handleAddSubmit)
+
+
+
 
 html.other.grid.addEventListener('click', handleEditToggle)
 html.edit.cancel.addEventListener('click', handleEditToggle)
 html.edit.form.addEventListener('submit', handleEditSubmit)
 html.edit.delete.addEventListener('click', handleDelete)
 
-html.help.cancel.addEventListener('click', handleHelpToggle)
-html.other.help.addEventListener('click', handleHelpToggle)
+
+
 
 for (const htmlColumn of Object.values(html.columns)) {
     htmlColumn.addEventListener('dragstart', handleDragStart)
